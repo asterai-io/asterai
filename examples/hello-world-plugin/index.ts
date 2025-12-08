05af2d2066a8925a4c863646d906af9a7a4a4339
Lorenzo Fontoura <lorenzo@nioel.com>
2025-12-08 22:30:13 +1000

    chore: remove legacy examples

--- examples/hello-world-plugin/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "@asterai/sdk";
-import { PluginInput, PluginOutput } from "@asterai/sdk";
-
-export function processMessage(input: PluginInput): PluginOutput | null {
-  return new PluginOutput("hello world plugin")
-    .withData("isHelloWorldPlugin", "yes, it is!")
-    .withAssistantMessage(`Hello, world! input: ${input.content}`);
-}
