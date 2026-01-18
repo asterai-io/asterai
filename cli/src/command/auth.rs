aec25d8a350eb83df9bd603e2c8c0319625a2b73
Lorenzo Fontoura <lorenzo@nioel.com>
2026-01-19 00:49:34 +1000

    chore: adjust auth command to match help.txt

--- cli/src/command/auth.rs
+++ cli/src/command/auth.rs
@@ -5,31 +5,46 @@ pub struct AuthArgs {
     action: AuthAction,
 }
 
-enum AuthAction {
-    Clear,
-    Set(String),
+pub enum AuthAction {
+    Login(String),
+    Logout,
+    Status,
 }
 
 impl AuthArgs {
     pub fn parse(mut args: impl Iterator<Item = String>) -> eyre::Result<Self> {
-        let content = args.collect::<String>().trim().to_owned();
-        if content.is_empty() {
-            bail!("missing argument. To clear your API key, use `auth clear`");
+        let subcommand = args
+            .next()
+            .ok_or_else(|| eyre::eyre!("missing subcommand. Expected: login, logout, or status"))?;
+        match subcommand.as_str() {
+            "login" => {
+                let api_key = args
+                    .next()
+                    .ok_or_else(|| eyre::eyre!("missing <api-key> argument"))?;
+                Ok(Self {
+                    action: AuthAction::Login(api_key),
+                })
+            }
+            "logout" => Ok(Self {
+                action: AuthAction::Logout,
+            }),
+            "status" => Ok(Self {
+                action: AuthAction::Status,
+            }),
+            _ => bail!("invalid subcommand. Expected: login, logout, or status"),
         }
-        let action = match content == "clear" {
-            true => AuthAction::Clear,
-            false => AuthAction::Set(content),
-        };
-        Ok(Self { action })
     }
 
     pub async fn execute(&self) -> eyre::Result<()> {
         match &self.action {
-            AuthAction::Clear => {
+            AuthAction::Login(api_key) => {
+                Auth::store_api_key(api_key)?;
+            }
+            AuthAction::Logout => {
                 Auth::clear_api_key()?;
             }
-            AuthAction::Set(api_key) => {
-                Auth::store_api_key(api_key)?;
+            AuthAction::Status => {
+                unimplemented!()
             }
         }
         Ok(())
