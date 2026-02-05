54f0e63809789faa8b5de10966ab2c4431fe9432
Lorenzo Fontoura <lorenzo@nioel.com>
2026-02-06 06:13:08 +1000

    chore: remove old math example

--- component-examples/math/component.ts
+++
@@ -1,8 +0,0 @@
-import { Output } from "./typegen/component";
-
-export const add = (a: number, b: number): Output => {
-  console.log("running component. a + b = ", a + b);
-  return {
-    value: a + b,
-  };
-};
