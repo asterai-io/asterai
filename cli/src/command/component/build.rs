e3c6dc412aa09336fed86f314fa299eaecffa16e
Lorenzo Fontoura <lorenzo@nioel.com>
2026-02-06 04:27:28 +1000

    chore: remove pkg from build step

--- cli/src/command/component/build.rs
+++ cli/src/command/component/build.rs
@@ -1,5 +1,4 @@
 use crate::command::component::ComponentArgs;
-use crate::command::component::pkg::run_pkg;
 use crate::language;
 use eyre::{Context, OptionExt, bail};
 
@@ -21,12 +20,6 @@ impl ComponentArgs {
         let lang = language::detect(&cwd)
             .ok_or_eyre("could not detect component language in current directory")?;
         println!("Detected {} component", lang.name());
-        // Generate package.wasm and package.wit from the WIT file.
-        let wit_file = lang.get_wit_file_path(&cwd);
-        let pkg_wasm = lang.get_package_wasm_path(&cwd);
-        let pkg_wit = lang.get_package_wit_path(&cwd);
-        run_pkg(&wit_file, &pkg_wasm, Some(&pkg_wit), &self.api_endpoint).await?;
-        // Run the language-specific build.
         let wasm_path = lang.build_component(&cwd)?;
         println!("Component built at {:?}", wasm_path);
         Ok(())
