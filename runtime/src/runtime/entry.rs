84c42c6181fdd9ee606a519357b7083a7c919891
Lorenzo Fontoura <lorenzo@nioel.com>
2026-02-06 14:15:45 +1000

    feat: implement new host functions except call_component_function

--- runtime/src/runtime/entry.rs
+++ runtime/src/runtime/entry.rs
@@ -1,32 +1,146 @@
-//! Host entry points, called from the plugin module.
+//! Host entry points for the asterai host API.
+use crate::component::wit::ComponentInterface;
 use crate::runtime::env::HostEnv;
-use crate::runtime::wit_bindings::exports::asterai::host::api::RuntimeInfo;
-use eyre::eyre;
+use crate::runtime::wit_bindings::exports::asterai::host::api::{
+    CallError, CallErrorKind, ComponentInfo, RuntimeInfo,
+};
+use std::collections::HashSet;
 use std::future::Future;
 use wasmtime::StoreContextMut;
 use wasmtime::component::Linker;
 
 pub fn add_asterai_host_to_linker(linker: &mut Linker<HostEnv>) -> eyre::Result<()> {
-    let mut exported_instance = linker
-        .instance("asterai:host/api@0.1.0")
-        .map_err(|e| eyre!("{e:#?}"))?;
-    // TODO allow a sink to forward these errors somewhere?
-    exported_instance
+    let mut instance = linker
+        .instance("asterai:host/api@1.0.0")
+        .map_err(|e| eyre::eyre!("{e:#?}"))?;
+    instance
         .func_wrap_async("get-runtime-info", get_runtime_info)
-        .map_err(|e| eyre!("{e:#?}"))?;
+        .map_err(|e| eyre::eyre!("{e:#?}"))?;
+    instance
+        .func_wrap_async("list-components", list_components)
+        .map_err(|e| eyre::eyre!("{e:#?}"))?;
+    instance
+        .func_wrap_async("list-other-components", list_other_components)
+        .map_err(|e| eyre::eyre!("{e:#?}"))?;
+    instance
+        .func_wrap_async("get-component", get_component)
+        .map_err(|e| eyre::eyre!("{e:#?}"))?;
+    instance
+        .func_wrap_async("component-implements", component_implements)
+        .map_err(|e| eyre::eyre!("{e:#?}"))?;
+    instance
+        .func_wrap_async("call-component-function", call_component_function)
+        .map_err(|e| eyre::eyre!("{e:#?}"))?;
     Ok(())
 }
 
 fn get_runtime_info<'a>(
-    store: StoreContextMut<'a, HostEnv>,
-    (_key, _user_id): (String, Option<String>),
-) -> Box<dyn Future<Output = eyre::Result<(RuntimeInfo,), wasmtime::Error>> + Send + 'a> {
+    _store: StoreContextMut<'a, HostEnv>,
+    _params: (),
+) -> Box<dyn Future<Output = Result<(RuntimeInfo,), wasmtime::Error>> + Send + 'a> {
     Box::new(async move {
-        let _runtime_metadata = store.data().runtime_data.as_ref().unwrap();
         let runtime_info = RuntimeInfo {
-            // TODO actually get version from metadata.
-            version: "todo-set-version".to_owned(),
+            version: env!("CARGO_PKG_VERSION").to_owned(),
         };
         Ok((runtime_info,))
     })
+}
+
+fn list_components<'a>(
+    store: StoreContextMut<'a, HostEnv>,
+    _params: (),
+) -> Box<dyn Future<Output = Result<(Vec<ComponentInfo>,), wasmtime::Error>> + Send + 'a> {
+    Box::new(async move {
+        let infos = build_all_component_infos(&store);
+        Ok((infos,))
+    })
+}
+
+fn list_other_components<'a>(
+    store: StoreContextMut<'a, HostEnv>,
+    _params: (),
+) -> Box<dyn Future<Output = Result<(Vec<ComponentInfo>,), wasmtime::Error>> + Send + 'a> {
+    Box::new(async move {
+        let caller_id = get_last_component_id(&store);
+        let infos = build_all_component_infos(&store)
+            .into_iter()
+            .filter(|info| Some(&info.name) != caller_id.as_ref())
+            .collect();
+        Ok((infos,))
+    })
+}
+
+fn get_component<'a>(
+    store: StoreContextMut<'a, HostEnv>,
+    (name,): (String,),
+) -> Box<dyn Future<Output = Result<(Option<ComponentInfo>,), wasmtime::Error>> + Send + 'a> {
+    Box::new(async move {
+        let info = build_all_component_infos(&store)
+            .into_iter()
+            .find(|info| info.name == name);
+        Ok((info,))
+    })
+}
+
+fn component_implements<'a>(
+    store: StoreContextMut<'a, HostEnv>,
+    (component_name, interface_name): (String, String),
+) -> Box<dyn Future<Output = Result<(bool,), wasmtime::Error>> + Send + 'a> {
+    Box::new(async move {
+        let found = build_all_component_infos(&store)
+            .into_iter()
+            .find(|info| info.name == component_name)
+            .is_some_and(|info| info.interfaces.contains(&interface_name));
+        Ok((found,))
+    })
+}
+
+fn call_component_function<'a>(
+    _store: StoreContextMut<'a, HostEnv>,
+    (_component_name, _function_name, _args_json): (String, String, String),
+) -> Box<dyn Future<Output = Result<(Result<String, CallError>,), wasmtime::Error>> + Send + 'a> {
+    Box::new(async move {
+        Ok((Err(CallError {
+            kind: CallErrorKind::InvocationFailed,
+            message: "not yet implemented".to_owned(),
+        }),))
+    })
+}
+
+fn build_all_component_infos(store: &StoreContextMut<HostEnv>) -> Vec<ComponentInfo> {
+    let Some(runtime_data) = store.data().runtime_data.as_ref() else {
+        return Vec::new();
+    };
+    runtime_data
+        .instances
+        .iter()
+        .map(|instance| {
+            let component = instance.component_interface.component();
+            let interfaces: Vec<String> = instance
+                .component_interface
+                .exported_interfaces()
+                .into_iter()
+                .map(|e| e.name)
+                .collect::<HashSet<_>>()
+                .into_iter()
+                .collect();
+            ComponentInfo {
+                name: component.id().to_string(),
+                version: component.version().to_string(),
+                interfaces,
+            }
+        })
+        .collect()
+}
+
+fn get_last_component_id(store: &StoreContextMut<HostEnv>) -> Option<String> {
+    store
+        .data()
+        .runtime_data
+        .as_ref()?
+        .last_component
+        .lock()
+        .unwrap()
+        .as_ref()
+        .map(|c| c.id().to_string())
 }
